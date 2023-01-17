3cb617bc9b8a06fa0f077eb2cbf55e4b70d68ab2
webdevelopersinkenya <mirr22006@byui.edu>
2023-01-17 14:28:21 +0300

    changes

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,12 +4,6 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.Write("What is your first name? ");
-        string first = Console.ReadLine();
-
-        Console.Write("What is your last name? ");
-        string last = Console.ReadLine();
-
-        Console.WriteLine($"Your name is {last}, {first} {last}.");
+        Console.WriteLine("Hello Prep1 World!");
     }
 }
