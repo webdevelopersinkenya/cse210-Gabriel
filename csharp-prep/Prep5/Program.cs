b5d5272b9d5bbe32c0099bb19cc8efbb3492d26b
webdevelopersinkenya <mirr22006@byui.edu>
2023-02-08 17:29:23 +0300

    Scripture Memorizer Program

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -1,9 +1,52 @@
 using System;
 
-class Program
+public class Scripture
 {
-    static void Main(string[] args)
+    private string book;
+    private int chapter;
+    private int verse;
+    private string text;
+
+    public string Book
     {
-        Console.WriteLine("Hello Prep5 World!");
+        get { return book; }
+        set { book = value; }
+    }
+
+    public int Chapter
+    {
+        get { return chapter; }
+        set { chapter = value; }
+    }
+
+    public int Verse
+    {
+        get { return verse; }
+        set { verse = value; }
+    }
+
+    public string Text
+    {
+        get { return text; }
+        set { text = value; }
+    }
+
+    public void Display()
+    {
+        Console.WriteLine(book + " " + chapter + ":" + verse + " - " + text);
+    }
+}
+
+public class HelloWorld
+{
+    public static void Main(string[] args)
+    {
+        Scripture scripture = new Scripture();
+        scripture.Book = "Proverb";
+        scripture.Chapter = 3;
+        scripture.Verse = 5 - 6;
+        scripture.Text = "Trust in the Lorad with all thine heart and leane not unto thine own understanding; In all thine ways acknowledge Him and He shall direct thy paths.";
+
+        scripture.Display();
     }
 }
